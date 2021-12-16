322fecee3fb2f113fb454ae6b73e6d07efe33edd
Rene-Zaltbommel <91453184+Rene-Zaltbommel@users.noreply.github.com>
2021-12-16 11:37:13 +0100

    Add files via upload

---
+++ ZevendeJavaFunctieArgumenten/index.js
@@ -0,0 +1,12 @@
+const paintWall = function (color,compas){
+    console.log('The wall has been painted' + color);
+    console.log('The'+ compas +'wall has been painted orange');
+
+};
+
+const color= ' blue ';
+const compas=' north ';
+
+
+
+paintWall(color,compas);
